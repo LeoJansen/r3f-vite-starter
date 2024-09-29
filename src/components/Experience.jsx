dd5312d62d4b3f80c6b4ce320da06e07ce52936a
Léo Jansen <leo@leoo.app>
2024-09-29 06:26:47 -0300

    First commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "../../DevAvatar";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1} >
+        <Avatar />
+
+      </group>
+
+      <ambientLight intensity={2} />
+
     </>
   );
 };

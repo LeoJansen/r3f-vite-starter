8d9de5e85fa33ccc58b35d0b3da6f7de761df73a
Léo Jansen <leo@leonardojansen.com>
2024-09-30 14:29:05 -0300

    Update animations

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,20 @@
 import { OrbitControls } from "@react-three/drei";
-import { Avatar } from "../../DevAvatar";
+import { Avatar } from "./DevAvatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+
+  const {animation} = useControls({
+    animation: { 
+    value: "typing",
+    options: ["typing", "salute", "idle", "fallingIdle", "fallingToLanding"]
+    }
+  });
   return (
     <>
       <OrbitControls />
       <group position-y={-1} >
-        <Avatar />
+        <Avatar animation={animation} />
 
       </group>
 
